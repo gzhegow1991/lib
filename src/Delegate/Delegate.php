c3bf1998fbd6096053f5a125a9ae236f042535a9
gzhegow <6562680@gmail.com>
2025-01-17 18:29:51 +0300

    main

--- src/Delegate/Delegate.php
+++ src/Delegate/Delegate.php
@@ -10,7 +10,7 @@ class Delegate
     /**
      * @var T
      */
-    protected $delegate;
+    protected $object;
 
 
     /**
@@ -18,7 +18,13 @@ class Delegate
      */
     public function __construct(object $delegate)
     {
-        $this->delegate = $delegate;
+        $this->object = $delegate;
+    }
+
+
+    public function getObject() : object
+    {
+        return $this->object;
     }
 
 
@@ -26,34 +32,34 @@ class Delegate
     {
         return (function ($name) {
             return isset($this->{$name});
-        })->call($this->delegate, $name);
+        })->call($this->object, $name);
     }
 
     public function __get($name)
     {
         return (function ($name) {
             return $this->{$name};
-        })->call($this->delegate, $name);
+        })->call($this->object, $name);
     }
 
     public function __set($name, $value)
     {
         (function ($name, $value) {
             $this->{$name} = $value;
-        })->call($this->delegate, $name, $value);
+        })->call($this->object, $name, $value);
     }
 
     public function __unset($name)
     {
         (function ($name) {
             unset($this->{$name});
-        })->call($this->delegate, $name);
+        })->call($this->object, $name);
     }
 
     public function __call($name, $arguments)
     {
         return (function ($name, $arguments) {
             return call_user_func_array([ $this, $name ], $arguments);
-        })->call($this->delegate, $name, $arguments);
+        })->call($this->object, $name, $arguments);
     }
 }
